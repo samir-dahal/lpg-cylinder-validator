ca53ddc8ec83c2d5f128af30f883d8947d9684f0
Samir Dahal <samir.dahal@sea.sg>
2025-04-18 11:21:22 +0545

    chore: add manifest for pwa

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'cylinder-checker-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/icon512_maskable.png',
+    '/icon512_rounded.png'
+];
+
+self.addEventListener('install', (e) =>
+{
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) =>
+{
+    e.respondWith(
+        caches.match(e.request)
+            .then(response => response || fetch(e.request))
+    );
+});
